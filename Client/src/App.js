75f23a61ef97eeeb7f2a0ef43a0bf604c5fe3178
Matias Montini <91911135+Matiasmo97@users.noreply.github.com>
2022-03-28 19:37:10 -0300

    Home

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import GameListRoom from './components/GAME-LIST-ROOM/gameListRoom';
+import FormAddQuestions from './components/HOME/FormAddQuestions/form';
+import Home from './components/HOME/home';
+import PlayStore from './components/PLAYSTORE/playstore';
+import PreGameRoom from './components/PRE-GAMEROOM/preGameRoom';
 
 function App() {
   return (
@@ -8,8 +13,12 @@ function App() {
         {/* <Route path="/" component={lading} /> */}
         {/* <Route path="/" component={login} /> */}
         {/* <Route path="/" component={registro} /> */}
-        {/* <Route path="/" component={home} /> */}
+        <Route path="/home" component={Home} />
         {/* <Route path="/" component={editPerfil} /> */}
+        <Route path="/iniciarPartida" component={PreGameRoom} />
+        <Route path="/partidasDisponibles" component={GameListRoom} />
+        <Route path="/tienda" component={PlayStore} />
+        <Route path="/añadirPregunta" component={FormAddQuestions} />
     </div>
   );
 }
---
+++ Client/src/redux/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
