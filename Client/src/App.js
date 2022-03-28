d017fbdbc72f290968b56e07eb97aad2a27b31a9
Marcos Davila <55298236+MarcosDavila1@users.noreply.github.com>
2022-03-28 19:28:35 -0300

    Firebase Login y SignUp
    Firebase Login y SignUp

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import { config } from './utils/Firebase';
+import { initializeApp } from 'firebase/app';
+import initialPage from './components/INICIO/initialPage';
+import signUpFirebase from './components/INICIO/SIGNUP/signUpFirebase';
+
+initializeApp(config)
 
 function App() {
   return (
     <div className="App">
         {/* <Route path="/" component={lading} /> */}
-        {/* <Route path="/" component={login} /> */}
-        {/* <Route path="/" component={registro} /> */}
+        <Route path="/login" component={initialPage} />
+        <Route path="/signup" component={signUpFirebase} />
         {/* <Route path="/" component={home} /> */}
         {/* <Route path="/" component={editPerfil} /> */}
     </div>
