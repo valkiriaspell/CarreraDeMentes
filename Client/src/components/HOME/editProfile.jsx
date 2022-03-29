7a14f81cd9e1f5a6135feeef01e5a225e6480ad1
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-03-29 11:20:00 -0300

    cambiarDatos
    casi listo, falta info de avatars

--- Client/src/components/HOME/editProfile.jsx
+++ Client/src/components/HOME/editProfile.jsx
@@ -8,72 +8,59 @@ import "../STYLES/editProfile.css"
 export default function EditProfile() {
     const dispatch = useDispatch()
     const history = useHistory();
-    
-    
+    const autenticado = localStorage.getItem('token')
+
     //////////  ---->    Local states data   <------ //////////////
     const [msg, setMSG] = useState('');
     const [username, setUsername] = useState("");
-    const [pass, setPass] = useState("");
-    const [pass2, setPass2] = useState("");
-    const [currentPass, setCurrentPass] = useState("");
     const [mail, setMail] = useState("");
     const [avatar, setAvatar] = useState("");
-    
+
     //////////  ---->    Local states errors   <------ //////////////
     const [errorName, setErrorName] = useState("")
-    const [errorPass, setErrorPass] = useState("")
-    const [errorPass2, setErrorPass2] = useState("")
-    const [errorMail, setErrorMail] = useState("")    
-    const [errorCurrentP, setErrorCurrentP] = useState("")
+    const [errorMail, setErrorMail] = useState("")
 
-    //////////  ---->    Store states    <------ //////////////
-    //necesito traerme la pass actual de este usuario... para comparar en el form.
-    const examplePass = "123" 
+
+    /////////  ---->    Store states    <------ //////////////
+    
+    //necesito traer los avatars
+
+
 
     //////////////// ---->    VALIDATIONS    <------ /////////////
 
-  function validation(e) {
-    switch (true) {
+    function validation(e) {
+        switch (true) {
 
-        case e.target.name === "name":
-          if (!/^[a-zA-Z ]*$/.test(e.target.value)) {
-            setUsername(e.target.value);
-            setErrorName("Escribir solo letras")
-          } else {
-            setUsername(e.target.value);
-            setErrorName("")
-          }
-          break;
-          case e.target.name === "mail":
-            if (!/\S+@\S+\.\S+/.test(e.target.value)) {
-              setMail(e.target.value);
-              setErrorMail("Se requiere un e-mail válido")
-            } else {
-              setMail(e.target.value);
-              setErrorMail("")
-            }
-            break;
-            case e.target.name === "currentP":
-                if (e.target.value !== examplePass ) {
-                  setCurrentPass(e.target.value);
-                  setErrorCurrentP("Clave incorrecta")
+            case e.target.name === "name":
+                if (!/^[a-zA-Z ]*$/.test(e.target.value)) {
+                    setUsername(e.target.value);
+                    setErrorName("Escribir solo letras")
                 } else {
-                  setCurrentPass(e.target.value);
-                  setErrorCurrentP("")
+                    setUsername(e.target.value);
+                    setErrorName("")
+                }
+                break;
+            case e.target.name === "mail":
+                if (!/\S+@\S+\.\S+/.test(e.target.value)) {
+                    setMail(e.target.value);
+                    setErrorMail("Se requiere un e-mail válido")
+                } else {
+                    setMail(e.target.value);
+                    setErrorMail("")
                 }
                 break;
             default:
-        console.log("default");
-  }
-}
+                console.log("default");
+        }
+    }
 
     //////////  ---->    on Submit   <------ //////////////
     const onSubmit = (e) => {
         e.preventDefault()
         // dispatch(updateUser({
         //     name: username,
-        //     avatar: avatar,
-        //     newpass: pass?pass:null,            
+        //     avatar: avatar,               
         //     mail: mail            
         // }))
         setMSG("Cambios guardados")
@@ -82,56 +69,39 @@ export default function EditProfile() {
         // setTimeout(() => { history.push('/home') }, 5000);
 
     }
+    if (autenticado) {
+        return (
+            <div className='formProfile'>
+                <form onSubmit={onSubmit}>
 
-    return (
-        <div className='formProfile'>
-            <form onSubmit={onSubmit}>
-
-                <div className='formName'>
-                    <label>Nombre de Usuario:</label>
-                    <>
-                    <input className={errorName !== "" ? 'danger' : "inputName"} name="name" type="text" value={username} onChange={(e) => validation(e)} />
-                    {errorName ? <p className='error'>{errorName}</p> : null}
-                    </>
-                </div>
-                <div className='formMail'>
-                    <label>E-Mail:</label>
-                    <div>
-                    <input className={errorMail !== "" ? 'danger' : "inputEmail"} name="mail" type="text" value={mail} onChange={(e) => validation(e)} />
-                    {errorMail ? <p className='error'>{errorMail}</p> : null}
+                    <div className='formName'>
+                        <label>Nombre de Usuario:</label>
+                        <>
+                            <input className={errorName !== "" ? 'danger' : "inputName"} name="name" type="text" value={username} onChange={(e) => validation(e)} />
+                            {errorName ? <p className='error'>{errorName}</p> : null}
+                        </>
                     </div>
-                </div>
-                <div className='formAvatar'>
-                    <label>Avatar</label>
-                    <div>
-                    <label>Carrousel de avatars</label>                    
+                    <div className='formMail'>
+                        <label>E-Mail:</label>
+                        <div>
+                            <input className={errorMail !== "" ? 'danger' : "inputEmail"} name="mail" type="text" value={mail} onChange={(e) => validation(e)} />
+                            {errorMail ? <p className='error'>{errorMail}</p> : null}
+                        </div>
                     </div>
-                </div>
-               
-                <div className='formPass'>
-                <div>
-                    <div>
-                <label>Para cambio de contraseña</label>
-                </div>
-                    <label>Contraseña actual:</label>
-                    <input className={errorCurrentP !== "" ? 'danger' : "inputPass"} name="currentP" type="password" value={currentPass} onChange={(e) => validation(e)} />
-                    {errorCurrentP ? <p className='error'>{errorCurrentP}</p> : null}
-                </div>
-                <div className='formNewPass'>
-                    <label>Nueva contraseña:</label>
-                    <input className={errorPass !== "" ? 'danger' : "inputPass"} name="newP" type="password" value={pass}  onChange={(e) => validation(e)} />
-                    {errorPass ? <p className='error'>{errorPass}</p> : null}
-                </div>
-                <div className='formConfirmPass'>
-                    <label>Confirmar contraseña:</label>
-                    <input className={errorPass2 !== "" ? 'danger' : "inputPass"} name="confirmP" type="password" value={pass2}  onChange={(e) => validation(e)} />
-                    {errorPass2 ? <p className='error'>{errorPass2}</p> : null}
-                </div>
-                </div>
-                <div className='Profilesubmit'>
-                <input disabled={errorName || errorMail} className={errorName ? "disabled" : "enabled"} type="submit" value="Guardar cambios"/>
-                </div>
-            </form>
-        </div>
-    );
+                    <div className='formAvatar'>
+                        <label>Avatar</label>
+                        <div>
+                            <label>Carrousel de avatars</label>
+                        </div>
+                    </div>
+                    <div className='Profilesubmit'>
+                        <input disabled={errorName || errorMail} className={errorName ? "disabled" : "enabled"} type="submit" value="Guardar cambios" />
+                    </div>
+                </form>
+            </div>
+        );
+    } else {
+        history.push('/login')
+        return <div></div>
+    }
 }
