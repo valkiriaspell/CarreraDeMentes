bbf843b1438a6cb10ec6571d197266b667713cb3
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-03-29 00:41:06 -0300

    working

--- Client/src/components/HOME/editProfile.jsx
+++ Client/src/components/HOME/editProfile.jsx
@@ -27,12 +27,45 @@ export default function EditProfile() {
     const [errorCurrentP, setErrorCurrentP] = useState("")
 
     //////////  ---->    Store states    <------ //////////////
-    //necesito traerme la pass actual de este usuario... para comparar en el form. 
+    //necesito traerme la pass actual de este usuario... para comparar en el form.
+    const examplePass = "123" 
 
     //////////////// ---->    VALIDATIONS    <------ /////////////
 
   function validation(e) {
+    switch (true) {
+
+        case e.target.name === "name":
+          if (!/^[a-zA-Z ]*$/.test(e.target.value)) {
+            setUsername(e.target.value);
+            setErrorName("Escribir solo letras")
+          } else {
+            setUsername(e.target.value);
+            setErrorName("")
+          }
+          break;
+          case e.target.name === "mail":
+            if (!/\S+@\S+\.\S+/.test(e.target.value)) {
+              setMail(e.target.value);
+              setErrorMail("Se requiere un e-mail válido")
+            } else {
+              setMail(e.target.value);
+              setErrorMail("")
+            }
+            break;
+            case e.target.name === "currentP":
+                if (e.target.value !== examplePass ) {
+                  setCurrentPass(e.target.value);
+                  setErrorCurrentP("Clave incorrecta")
+                } else {
+                  setCurrentPass(e.target.value);
+                  setErrorCurrentP("")
+                }
+                break;
+            default:
+        console.log("default");
   }
+}
 
     //////////  ---->    on Submit   <------ //////////////
     const onSubmit = (e) => {
@@ -56,22 +89,30 @@ export default function EditProfile() {
 
                 <div className='formName'>
                     <label>Nombre de Usuario:</label>
+                    <>
                     <input className={errorName !== "" ? 'danger' : "inputName"} name="name" type="text" value={username} onChange={(e) => validation(e)} />
                     {errorName ? <p className='error'>{errorName}</p> : null}
+                    </>
                 </div>
                 <div className='formMail'>
                     <label>E-Mail:</label>
-                    <input className={errorMail !== "" ? 'danger' : "inputEmail"} name="name" type="text" value={mail} onChange={(e) => validation(e)} />
+                    <div>
+                    <input className={errorMail !== "" ? 'danger' : "inputEmail"} name="mail" type="text" value={mail} onChange={(e) => validation(e)} />
                     {errorMail ? <p className='error'>{errorMail}</p> : null}
+                    </div>
                 </div>
                 <div className='formAvatar'>
                     <label>Avatar</label>
+                    <div>
                     <label>Carrousel de avatars</label>                    
+                    </div>
                 </div>
                
                 <div className='formPass'>
                 <div>
+                    <div>
                 <label>Para cambio de contraseña</label>
+                </div>
                     <label>Contraseña actual:</label>
                     <input className={errorCurrentP !== "" ? 'danger' : "inputPass"} name="currentP" type="password" value={currentPass} onChange={(e) => validation(e)} />
                     {errorCurrentP ? <p className='error'>{errorCurrentP}</p> : null}
@@ -86,6 +127,9 @@ export default function EditProfile() {
                     <input className={errorPass2 !== "" ? 'danger' : "inputPass"} name="confirmP" type="password" value={pass2}  onChange={(e) => validation(e)} />
                     {errorPass2 ? <p className='error'>{errorPass2}</p> : null}
                 </div>
+                </div>
+                <div className='Profilesubmit'>
+                <input disabled={errorName || errorMail} className={errorName ? "disabled" : "enabled"} type="submit" value="Guardar cambios"/>
                 </div>
             </form>
         </div>
