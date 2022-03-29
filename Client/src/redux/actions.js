974fa6ddfa0824726afac37968beff2fc0adcc98
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-03-28 22:37:11 -0300

    newActionUpdateUser

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
 export const LOGIN_USER_GUEST = 'LOGIN_USER_GUEST'
+export const UPDATE_USER = 'UPDATE_USER'
+
+
 
 export function LoginAsGuest(userGuest){
     return async function(dispatch){
         try{
-        const {data} = axios.post('/ruta para hacer post', {userGuest})
-        data => dispatch({type: 'LOGIN_USER_GUEST', payload: data})
+        const {data} = await axios.post('/ruta para hacer post', {userGuest})
+        return dispatch({type: LOGIN_USER_GUEST, payload: data})
         }catch(e) {
-            e => console.log(e)
+          return console.log(e)
         }
+    }
+}
 
+export const updateUser = (userData)=> async (dispatch)=>{
+    try {
+        const result = await axios.post(`/ ruta para actualizar user`, userData)  
+    } catch (error) {
+        console.log(error)
     }
 }
