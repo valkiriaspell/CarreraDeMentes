3a85af57906981bde9c2445fa5bf982d4f24e23a
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-03-29 23:31:33 -0300

    formsREADY

--- Client/src/components/HOME/editProfile.jsx
+++ Client/src/components/HOME/editProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import { updateUser } from '../../redux/actions';
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import "../STYLES/editProfile.css"
 
 
@@ -24,6 +24,7 @@ export default function EditProfile() {
     /////////  ---->    Store states    <------ //////////////
     
     //necesito traer los avatars
+    let falseAvatars = ["avatar1","avatar2","avatar3","avatar4"]
 
 
 
@@ -68,6 +69,8 @@ export default function EditProfile() {
         //     mail: mail            
         // }))
         setMSG("Cambios guardados")
+        setUsername("")
+        setMail("")
         // setTimeout(() => { setMSG("...Going back to Home now") }, 3000);
 
         // setTimeout(() => { history.push('/home') }, 5000);
@@ -77,7 +80,7 @@ export default function EditProfile() {
         return (
             <div className='formProfile'>
                 <form onSubmit={onSubmit}>
-                <label>Editar perfil</label>                
+                <h3>Editar perfil</h3>                
                     <div className='formName'>
                         <label>Nombre de Usuario:</label>
                         <>
@@ -101,7 +104,9 @@ export default function EditProfile() {
                     <div className='Profilesubmit'>
                         <input disabled={errorName || errorMail} className={errorName ? "disabled" : "enabled"} type="submit" value="Guardar cambios" />
                     </div>
+                    {msg ? <p>{msg}</p> : null}
                 </form>
+                <Link to="/home"><button className='volver' >← Volver atras </button></Link>
             </div>
         );
     } else {
