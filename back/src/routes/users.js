1f1c62db6a1bc4020efb9cd8f2c3f36e958336f6
leandroveroninf <87210402+leandroveroninf@users.noreply.github.com>
2022-03-30 12:12:35 -0300

    gameRoom

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -4,11 +4,6 @@ module.exports = router;
 const {GameRoom, Question, Users, Chat, Avatar} = require('../db.js')
 
 // escriban sus rutas acá
-router.post('/',async function(req,res){ 
-   
-
-})
-
 router.get("/", async(req, res)=>{
    
 })
