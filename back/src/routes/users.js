348e2190d42e234514ca624a0bcb86b23db290b6
leandroveroninf <87210402+leandroveroninf@users.noreply.github.com>
2022-03-30 12:06:14 -0300

    gameRoon

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -5,16 +5,10 @@ const {GameRoom, Question, Users, Chat, Avatar} = require('../db.js')
 
 // escriban sus rutas acá
 router.post('/',async function(req,res){ 
-    
-    const {name, email} = req.body;
-    await Users.create({name, email});
-
-    res.send("creado")
+   
 
 })
 
 router.get("/", async(req, res)=>{
-    const data = await Users.findAll();
-
-    res.send(data);
+   
 })
