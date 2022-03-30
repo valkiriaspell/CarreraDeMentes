2b691f5301328ce9d122071b00ae6a11b6e0ef41
Eduardo Andres Abelardo <88063168+eduabelardo@users.noreply.github.com>
2022-03-30 11:21:44 -0300

    Users route avance
    avance en las rutas de users

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-var express = require('express');
-var router = express.Router();
-module.exports = router;
-const {GameRoom, Question, Users, Chat, Avatar} = require('../db.js')
-
-// escriban sus rutas acá
-router.get('/',async function(req,res){ 
-})
-=======
 const {Router} = require('express');
 const router = Router();
 const {createUsers, getUser, getUsers} = require('../controllers/users');
@@ -15,4 +5,3 @@ const {createUsers, getUser, getUsers} = require('../controllers/users');
 router.use('/', createUsers);
 
 module.exports = router;
->>>>>>> Stashed changes
