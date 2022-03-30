a5b0d3ff58770728e9359f382aa9c0e0b384682d
Eduardo Andres Abelardo <88063168+eduabelardo@users.noreply.github.com>
2022-03-30 11:20:16 -0300

    Users route avance
    avance en las rutas de users

---
+++ back/src/controllers/users.js
@@ -0,0 +1,62 @@
+const {Users} = require('../db');
+
+const createUsers = async (
+	id,
+	name,
+	email,
+	currentAvatar,
+	avatarStock,
+	coins,
+	experiencePoints,
+	level,
+	wins,
+	friendId,
+	host,
+	guest
+) => {
+	try {
+		const newUser = await Users.findOrCreate({
+			id,
+			name,
+			email,
+			currentAvatar,
+			avatarStock,
+			coins,
+			experiencePoints,
+			level,
+			wins,
+			friendId,
+			host,
+			guest,
+		});
+
+		return newUser;
+	} catch (error) {
+		return error;
+	}
+};
+
+const getUser = async (email) => {
+	try {
+		let userFound = await Users.findAll({where: {email: email}});
+
+		return userFound;
+	} catch (error) {
+		return error;
+	}
+};
+const getUsers = async () => {
+	try {
+		let usersFound = await Users.findAll();
+
+		return usersFound;
+	} catch (error) {
+		return error;
+	}
+};
+
+module.exports = {
+	createUsers,
+	getUser,
+	getUsers,
+};
--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var express = require('express');
 var router = express.Router();
 module.exports = router;
@@ -6,3 +7,12 @@ const {GameRoom, Question, Users, Chat, Avatar} = require('../db.js')
 // escriban sus rutas acá
 router.get('/',async function(req,res){ 
 })
+=======
+const {Router} = require('express');
+const router = Router();
+const {createUsers, getUser, getUsers} = require('../controllers/users');
+
+router.use('/', createUsers);
+
+module.exports = router;
+>>>>>>> Stashed changes
