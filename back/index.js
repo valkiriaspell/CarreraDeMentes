f8a5c464f4dd0c3f55fdbf5c4fa1c4036db4c5f9
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-03-31 18:37:46 -0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const app = require('./src/app.js');
 const {conn} = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({force: true}).then(() => {
+conn.sync({force: false}).then(() => {
 	app.listen(process.env.PORT || 3001, () => {
 		console.log('%s listening at 3001'); // eslint-disable-line no-console
 	});
