04178df0f2dbc3c9e0742e2b1bb638bf66dc9244
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-04-02 00:49:29 -0300

    carrouselAvatars
    casi listo

--- Client/src/components/HOME/editProfile.jsx
+++ Client/src/components/HOME/editProfile.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import { updateUser } from '../../redux/actions';
 import { Link, useHistory } from 'react-router-dom'
+import { getAvatars } from '../../redux/actions';
+import Avatars from '../AVATARS/avatars';
 import "../STYLES/form.css"
 
 
@@ -12,22 +14,22 @@ export default function EditProfile() {
 
     //////////  ---->    Local states data   <------ //////////////
     const [msg, setMSG] = useState("");
-    const [username, setUsername] = useState("");
-    const [mail, setMail] = useState("");
+    const [username, setUsername] = useState("");    
     const [avatar, setAvatar] = useState("");
 
     //////////  ---->    Local states errors   <------ //////////////
-    const [errorName, setErrorName] = useState("")
-    const [errorMail, setErrorMail] = useState("")
+    const [errorName, setErrorName] = useState("")    
 
 
     /////////  ---->    Store states    <------ //////////////
     
-    //necesito traer los avatars
-    let falseAvatars = ["avatar1","avatar2","avatar3","avatar4"]
+    useEffect(() => {
+        dispatch(getAvatars()) 
+    }, [])
+    
+    
 
-
-
+    
     //////////////// ---->    VALIDATIONS    <------ /////////////
 
     function validation(e) {
@@ -45,16 +47,7 @@ export default function EditProfile() {
                     setUsername(e.target.value);
                     setErrorName("")
                 }
-                break;
-            case e.target.name === "mail":
-                if (!/\S+@\S+\.\S+/.test(e.target.value)) {
-                    setMail(e.target.value);
-                    setErrorMail("Se requiere un e-mail válido")
-                } else {
-                    setMail(e.target.value);
-                    setErrorMail("")
-                }
-                break;
+                break;           
             default:
                 console.log("default");
         }
@@ -70,15 +63,13 @@ export default function EditProfile() {
         // }))
         setMSG("Cambios guardados")
         setUsername("")
-        setMail("")
-        // setTimeout(() => { setMSG("...Going back to Home now") }, 3000);
-
-        // setTimeout(() => { history.push('/home') }, 5000);
-
+     
+      
     }
     if (autenticado) {
         return (
             <div className='form'>
+            
                 <form onSubmit={onSubmit}>
                 <h3>Editar perfil</h3>                
                     <div className='formName'>
@@ -87,26 +78,19 @@ export default function EditProfile() {
                             <input className={errorName !== "" ? 'danger' : "inputName"} name="name" type="text" value={username} onChange={(e) => validation(e)} />
                             {errorName ? <p className='error'>{errorName}</p> : null}
                         </>
-                    </div>
-                    <div className='formMail'>
-                        <label>E-Mail:</label>
-                        <div>
-                            <input className={errorMail !== "" ? 'danger' : "inputEmail"} name="mail" type="text" value={mail} onChange={(e) => validation(e)} />
-                            {errorMail ? <p className='error'>{errorMail}</p> : null}
-                        </div>
-                    </div>
+                    </div>                   
                     <div className='formAvatar'>
                         <label>Avatar</label>
                         <div>
-                            <label>Carrousel de avatars</label>
+                            <Avatars />
                         </div>
                     </div>
                     <div className='FormSubmit'>
-                        <input disabled={errorName || errorMail || !username && !mail && !avatar} className={errorName || errorMail || !username && !mail && !avatar ? "disabled" : "enabled"} type="submit" value="Guardar cambios" />
-                        
-                    </div>
+                        <input disabled={errorName || !username && !avatar} className={errorName || !username && !avatar ? "disabled" : "enabled"} type="submit" value="Guardar cambios" />
+                    </div>                        
                     {msg ? <p>{msg}</p> : null}
                 </form>
+                
                 <Link to="/home"><button className='volver' >← Volver atras </button></Link>
             </div>
         );
---
+++ client/src/components/AVATARS/item.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 200px;
+  background-color: #00008B;
+  color: #fff;
+  margin: 0 15px;
+  font-size: 4em;
+`;
