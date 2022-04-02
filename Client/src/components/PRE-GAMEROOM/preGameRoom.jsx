954a7427ddfb3d00018b144c28a5a7281f92df6d
Matias Montini <91911135+Matiasmo97@users.noreply.github.com>
2022-04-02 18:51:01 -0300

    Estructura preGameRoom

--- Client/src/components/PRE-GAMEROOM/preGameRoom.jsx
+++ Client/src/components/PRE-GAMEROOM/preGameRoom.jsx
@@ -29,7 +29,7 @@ function PreGameRoom({match}) {
                     <div>
                     <ListPlayers expelPlayer={expelPlayer} />
                     </div>
-                    <div className={s.container2}>
+                    <div>
                     <div>
                     <EditRoom />
                     </div>
@@ -37,7 +37,7 @@ function PreGameRoom({match}) {
                     <Chat idUser={idUser}/>
                     </div>
                     
-                    <div>
+                    <div className={s.buttonsPreGameRoom}>
                     {
                         user.host === true
                             ? (<button 
