855a4596b79fe2e423a3d3ceef2a2758cca85fa0
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-04-07 13:28:46 -0300

    modificandoDeploy

--- back/index.js
+++ back/index.js
@@ -7,13 +7,13 @@ const { SUPERADMIN_NAME, SUPERADMIN_EMAIL, SUPERADMIN_PASS, SUPERADMIN_ADMIN } =
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
 	app.listen(process.env.PORT || 3001, async () => {
-		if (process.env.PORT){
+		// if (process.env.PORT){
 			await axios.get('/avatar');
 			await axios.get('/coins/multiplesCoins');	
-		} else {
-			await axios.get('http://localhost:3001/avatar');
-			await axios.get('http://localhost:3001/coins/multiplesCoins');
-		}
+		// } else {
+		// 	await axios.get('http://localhost:3001/avatar');
+		// 	await axios.get('http://localhost:3001/coins/multiplesCoins');
+		// }
 		createUsers({
 			name: SUPERADMIN_NAME,
 			email: SUPERADMIN_EMAIL,
