fd1ccbf715a63b98e9a40a11f0a35d88e623bcd9
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-04-07 13:48:51 -0300

    funciones en index agregadas

--- back/index.js
+++ back/index.js
@@ -1,26 +1,23 @@
 const axios = require('axios');
 const app = require('./src/app.js');
+const { getAvatars } = require('./src/controllers/avatars.js');
+const { addCoins } = require('./src/controllers/coins.js');
 const { createUsers } = require('./src/controllers/users.js');
 const { conn } = require('./src/db.js');
 const { SUPERADMIN_NAME, SUPERADMIN_EMAIL, SUPERADMIN_PASS, SUPERADMIN_ADMIN } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-	app.listen(process.env.PORT, async () => {
-		// if (process.env.PORT){
-			// await axios.get('/avatar');
-			// await axios.get('/coins/multiplesCoins');	
-		// } else {
-		// 	await axios.get('http://localhost:3001/avatar');
-		// 	await axios.get('http://localhost:3001/coins/multiplesCoins');
-		// }
-		// createUsers({
-		// 	name: SUPERADMIN_NAME,
-		// 	email: SUPERADMIN_EMAIL,
-		// 	password: SUPERADMIN_PASS,
-		// 	idAvatar: '1',
-		// 	admin: SUPERADMIN_ADMIN,
-		// });
+	app.listen(process.env.PORT, async () => {	
+		getAvatars();
+		addCoins();		
+		createUsers({
+			name: SUPERADMIN_NAME,
+			email: SUPERADMIN_EMAIL,
+			password: SUPERADMIN_PASS,
+			idAvatar: '1',
+			admin: SUPERADMIN_ADMIN,
+		});
 		console.log('%s listening at PORT'); // eslint-disable-line no-console
 	});
 });
