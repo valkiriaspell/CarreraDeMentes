a6308f7cd81e0a431a3bdff429515c97f5b3dd4a
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-04-07 13:42:51 -0300

    quitando funciones de index

--- back/index.js
+++ back/index.js
@@ -8,19 +8,19 @@ const { SUPERADMIN_NAME, SUPERADMIN_EMAIL, SUPERADMIN_PASS, SUPERADMIN_ADMIN } =
 conn.sync({ force: false }).then(() => {
 	app.listen(process.env.PORT, async () => {
 		// if (process.env.PORT){
-			await axios.get('/avatar');
-			await axios.get('/coins/multiplesCoins');	
+			// await axios.get('/avatar');
+			// await axios.get('/coins/multiplesCoins');	
 		// } else {
 		// 	await axios.get('http://localhost:3001/avatar');
 		// 	await axios.get('http://localhost:3001/coins/multiplesCoins');
 		// }
-		createUsers({
-			name: SUPERADMIN_NAME,
-			email: SUPERADMIN_EMAIL,
-			password: SUPERADMIN_PASS,
-			idAvatar: '1',
-			admin: SUPERADMIN_ADMIN,
-		});
+		// createUsers({
+		// 	name: SUPERADMIN_NAME,
+		// 	email: SUPERADMIN_EMAIL,
+		// 	password: SUPERADMIN_PASS,
+		// 	idAvatar: '1',
+		// 	admin: SUPERADMIN_ADMIN,
+		// });
 		console.log('%s listening at PORT'); // eslint-disable-line no-console
 	});
 });
