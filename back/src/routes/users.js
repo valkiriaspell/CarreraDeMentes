f49f583863f575284924b6fdeeacb3b8232888f1
Eduardo Andres Abelardo <88063168+eduabelardo@users.noreply.github.com>
2022-04-07 05:59:42 -0300

    Rutas host modificadas
    Rutas host modificadas

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -96,9 +96,13 @@ router.delete('/', async (req, res) => {
 });
 router.put('/', async (req, res) => {
 	try {
-		const {email, host} = req.query;
+		const {id, email, host} = req.query;
 		if (email) {
 			const hostFound = await modifyHost(email, host);
+			res.send(hostFound);
+		}
+		if (id) {
+			const hostFound = await modifyHost(id, host);
 			res.send(hostFound);
 		} else {
 			const userUpdated = await modifyUser(req.body);
