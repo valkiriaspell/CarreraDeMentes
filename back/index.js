6afc67cbc5c4ed2969ddc8ec972ba38d648aaee5
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-04-10 15:42:12 -0300

    arreglandoDeploy

--- back/index.js
+++ back/index.js
@@ -1,17 +1,16 @@
-const axios = require('axios');
 const app = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { getAvatars } = require('./src/controllers/avatars.js');
-const { addInitServerMultCoins } = require('./src/controllers/coins.js');
+// const { addInitServerMultCoins } = require('./src/controllers/coins.js');
 const { createUsers } = require('./src/controllers/users.js');
 const { data } = require('./src/controllers/question.js');
-const { SUPERADMIN_NAME, SUPERADMIN_EMAIL, SUPERADMIN_PASS, SUPERADMIN_ADMIN } = process.env;
+const { SUPERADMIN_NAME, SUPERADMIN_EMAIL, SUPERADMIN_ADMIN } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
 	app.listen(process.env.PORT || 3001, async () => {
 		getAvatars();	
-		addInitServerMultCoins();	
+		// addInitServerMultCoins();	
 		data();
 		createUsers({
 			name: SUPERADMIN_NAME,
