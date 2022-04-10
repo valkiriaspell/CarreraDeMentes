9f6b678fa93496bfa1aa8f9179c080cf8b11c837
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-04-10 12:40:25 -0300

    endgame

---
+++ Client/src/components/GAMEROOM/endGame.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "../STYLES/endGame.css";
+
+
+function GameRoom({ preRoomUsers }) {
+
+    return (
+        <div className="containerEG">
+            <div className="scoreTable"></div>
+
+        </div>
+    )
+}
