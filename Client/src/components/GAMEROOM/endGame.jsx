f83f36d6da7f151794319e86443b5429e7b5e9a3
Matias Montini <91911135+Matiasmo97@users.noreply.github.com>
2022-04-12 16:21:37 -0300

    Gastar Coins en las Partidas

--- Client/src/components/GAMEROOM/endGame.jsx
+++ Client/src/components/GAMEROOM/endGame.jsx
@@ -1,36 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "../STYLES/endGame.css";
 
+export default function EndGame({ userCoins, user }) {
+    
+  useEffect(() => {
+    const getCoins = async () => {
+      await axios.post("http://localhost:3001/mercadopago", {
+        coinsFinal: userCoins,
+        email: user.email,
+      });
+    };
+    getCoins();
+  }, []);
 
-export default function EndGame() {
-
-    return (
-        <div className="containerEG">
-            <div className="scoreTable">
-                <div className="interior">
-                    <div className="resultados">                 
-                            <li>Puesto</li>
-                            <li>Jugador</li>
-                            <li>Puntos ganados</li>
-                            <li>Nivel</li>                           
-                    </div>
-                    <div className="resultados">                 
-                            <li>1°</li>
-                            <li>Player 1</li>
-                            <li>120</li>
-                            <li>5</li>                           
-                    </div>
-                    <div className="resultados">                 
-                            <li>1°</li>
-                            <li>Player 1</li>
-                            <li>120</li>
-                            <li>5</li>                           
-                    </div>
-                    </div>               
-                    
-                
-            </div>
-
+  return (
+    <div className="containerEG">
+      <div className="scoreTable">
+        <div className="interior">
+          <div className="resultados">
+            <li>Puesto</li>
+            <li>Jugador</li>
+            <li>Puntos ganados</li>
+            <li>Nivel</li>
+          </div>
+          <div className="resultados">
+            <li>1°</li>
+            <li>Player 1</li>
+            <li>120</li>
+            <li>5</li>
+          </div>
+          <div className="resultados">
+            <li>1°</li>
+            <li>Player 1</li>
+            <li>120</li>
+            <li>5</li>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
