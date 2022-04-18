7fc63737bffe66ac192d7f30819c97025d1ad741
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-04-18 12:26:27 -0300

    reiniciandoFALSE

--- back/index.js
+++ back/index.js
@@ -4,11 +4,11 @@ const {getAvatars} = require('./src/controllers/avatars.js');
 const {addInitServerMultCoins} = require('./src/controllers/coins.js');
 const {createUsers} = require('./src/controllers/users.js');
 const {data} = require('./src/controllers/question.js');
-const {SUPERADMIN_NAME, SUPERADMIN_EMAIL, SUPERADMIN_PASS, SUPERADMIN_ADMIN} =
+const {SUPERADMIN_NAME, SUPERADMIN_EMAIL, SUPERADMIN_ADMIN} =
 	process.env;
 
 // Syncing all the models at once.
-conn.sync({force: true}).then(() => {
+conn.sync({force: false}).then(() => {
 	app.listen(process.env.PORT || 3001, async () => {
 		getAvatars();
 		addInitServerMultCoins();		
